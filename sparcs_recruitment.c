60c8a3b1926e8a210b906056ba0548039164439b
Bea Totanes <36788217+beatricetotanes@users.noreply.github.com>
2022-03-11 21:31:38 +0800

    revert changes

--- sparcs_recruitment.c
+++ sparcs_recruitment.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
-void charmagneDuyag();
+void beaTotanes();
+void kayeJose();
 
 int main(){
     int choice;
@@ -19,19 +20,19 @@ int main(){
         switch (choice)
         {
             case 1:
-                // put function here
-                printf("Test 1\n");
+                kayeJose();
                 break;
             case 2:
                 // put function here
-                charmagneDuyag();
+                printf("Test 2\n");
                 break;
             case 3:
                 // put function here
                 printf("Test 3\n");
                 break;
             case 4:
-                beaTotanes();
+                // put function here
+                printf("Test 2\n");
                 break;
             case 5:
                 // put function here
@@ -44,13 +45,23 @@ int main(){
     
 }
 
-void charmagneDuyag(){
-    printf("\nName: Charmagne Jane Duyag\n");
-    printf("Nickname: Penpen\n");
+void beaTotanes(){
+    printf("\nName: Beatrice Totanes\n");
+    printf("Nickname: Bea\n");
+    printf("Course: BS Computer Science\n");
+    printf("Year: 3\n");
+    printf("Age: 21\n");
+    printf("Location: Davao City\n");
+    printf("Favorite Programming Languages: C++ & Dart\n");
+    printf("Hobbies: Reading & Watching Movies\n\n");
+}
+void kayeJose(){
+    printf("\nName: Kaye Jose\n");
+    printf("Nickname: Kay\n");
     printf("Course: BS Computer Science\n");
     printf("Year: 1\n");
-    printf("Age: 18\n");
-    printf("Location: General Santos City\n");
-    printf("Favorite Programming Language: Python\n");
-    printf("Hobbies: Reading, Watching TV Series, Listening to Music, & Journaling\n\n");
+    printf("Age: 19\n");
+    printf("Location: Davao City\n");
+    printf("Favorite Programming Languages: C, Python\n");
+    printf("Hobbies: Reading\n\n");
 }
